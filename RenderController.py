cd9b5b1a859ec8619c441d99579d0ca7e8f37206
Kevin Ollivier <kevino@theolliviers.com>
2013-04-14 19:15:01 +0000

    Add RenderController.py for updating UI based on layer change events.

---
+++ RenderController.py
@@ -0,0 +1,34 @@
+"""
+This class controls rendering.
+"""
+
+from wx.lib.pubsub import pub
+
+class RenderController( object ):
+    def __init__( self, layer_manager, render_window ):
+        self.layer_manager = layer_manager
+        self.render_window = render_window
+        
+        pub.subscribe( self.on_layer_points_lines_changed, ('layer', 'lines', 'changed') )
+        pub.subscribe( self.on_layer_points_lines_changed, ('layer', 'points', 'changed') )
+        pub.subscribe( self.on_layer_points_lines_changed, ('layer', 'points', 'deleted') )
+
+        pub.subscribe( self.on_projection_changed, ('layer', 'projection', 'changed') )
+        pub.subscribe( self.on_layer_updated, ('layer', 'updated' ) )
+        pub.subscribe( self.on_layer_triangulated, ('layer', 'triangulated') )
+        
+    def on_layer_updated( self, layer ):
+        if layer in self.layer_manager.layers:
+            self.render_window.update_renderers()
+        
+    def on_layer_points_lines_changed( self, layer ):
+        if layer in self.layer_manager.layers:
+            self.render_window.rebuild_points_and_lines_for_layer( layer )
+        
+    def on_projection_changed( self, layer, projection ):
+        if layer in self.layer_manager.layers:
+            self.render_window.reproject_all( projection )
+
+    def on_layer_triangulated( self, layer ):
+        if layer in self.layer_manager.layers:
+            self.render_window.rebuild_triangles_for_layer( layer )
