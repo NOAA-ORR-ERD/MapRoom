63aa9f0d219aa8d04d10e4241769e344f134f246
Rob McMullen <rob.mcmullen@noaa.gov>
2013-11-22 13:20:25 -0800

    Fixed #527: added flush method to Logger object for py2app std[out|err]

--- maproom.py
+++ maproom.py
@@ -35,6 +35,9 @@ class Logger(object):
     def write(self, message):
         self.terminal.write(message)
         self.log.write(message)
+    
+    def flush(self):
+        self.log.flush()
 
 
 def main():
