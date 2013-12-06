224198bdfe9df984e6ccfbdaaf23a6658f837e6d
Rob McMullen <rob.mcmullen@noaa.gov>
2013-12-06 11:27:08 -0800

    Refs #550: updated about dialog to show versions

--- ui/About_dialog.py
+++ ui/About_dialog.py
@@ -13,7 +13,6 @@ class About_dialog:
 
         info.SetIcon(wx.Icon(logo_filename, wx.BITMAP_TYPE_PNG))
         info.SetName("Maproom")
-        info.SetCopyright("Developed by NOAA")
 
         if not app_globals.version.SOURCE_CONTROL_REVISION:
             info.SetVersion(app_globals.version.VERSION)
@@ -22,7 +21,29 @@ class About_dialog:
                 app_globals.version.VERSION, app_globals.version.SOURCE_CONTROL_REVISION,
             ))
 
-        info.SetDescription("High-performance 2d mapping")
+        desc = "High-performance 2d mapping developed by NOAA\n\nUsing:\n"
+        desc += "  wxPython %s\n" % wx.version()
+        try:
+            import gdal
+            desc += "  GDAL %s\n" % gdal.VersionInfo()
+        except:
+            pass
+        try:
+            import numpy
+            desc += "  numpy %s\n" % numpy.version.version
+        except:
+            pass
+        try:
+            import OpenGL
+            desc += "  PyOpenGL %s\n" % OpenGL.__version__
+        except:
+            pass
+        try:
+            import pyproj
+            desc += "  PyProj %s\n" % pyproj.__version__
+        except:
+            pass
+        info.SetDescription(desc)
         info.SetWebSite("http://www.noaa.gov/")
 
         dialog = wx.AboutBox(info)
