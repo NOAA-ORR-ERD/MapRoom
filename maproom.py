dd993625a1fb5c6bb882d678f598fe3d8866052a
Rob McMullen <rob.mcmullen@noaa.gov>
2014-02-22 06:29:31 -0800

    Call new peppy2.framework.application.run function for startup

--- maproom.py
+++ maproom.py
@@ -1,19 +1,12 @@
 # Maproom application using the Peppy2 framework
 
 # Standard library imports.
-from pkg_resources import Environment, working_set
 import logging
 
-# Enthought library imports.
-from traits.etsconfig.api import ETSConfig
-from envisage.api import PluginManager
-from envisage.core_plugin import CorePlugin
-from envisage.ui.tasks.tasks_plugin import TasksPlugin
- 
+# Framework imports.
+from peppy2.framework.application import run
+
 # Local imports.
-from peppy2.framework.plugin import FrameworkPlugin
-from peppy2.file_type.plugin import FileTypePlugin
-from peppy2.framework.application import FrameworkApplication
 from maproom.plugin import MaproomPlugin
 
 # Imports for py2exe/py2app
@@ -31,19 +24,9 @@ def main(argv):
     logger = logging.getLogger()
     logger.setLevel(logging.DEBUG)
 
-    plugins = [ CorePlugin(), TasksPlugin(), FrameworkPlugin(), FileTypePlugin(), MaproomPlugin() ]
+    plugins = [ MaproomPlugin() ]
     
-    import peppy2.file_type.recognizers
-    plugins.extend(peppy2.file_type.recognizers.plugins)
-    
-    default = PluginManager(
-        plugins = plugins,
-    )
-    plugin_manager = default
-    
-    app = FrameworkApplication(plugin_manager=plugin_manager)
-    
-    app.run()
+    run(plugins=plugins, use_eggs=False)
 
     logging.shutdown()
 
