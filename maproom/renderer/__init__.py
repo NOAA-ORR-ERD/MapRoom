6d16dfbf8d476bf5ed6f0c7d986d37acad054109
Rob McMullen <rob.mcmullen@noaa.gov>
2014-04-10 11:26:33 -0700

    fixed data_types import

--- maproom/renderer/__init__.py
+++ maproom/renderer/__init__.py
@@ -2,4 +2,4 @@ from .gl import Opengl_renderer as RendererDriver
 from .gl.layer_renderer import LayerRenderer
 from .gl.parser import *
 from .gl.color import color_to_int, int_to_color
-import gl.data_types
+import gl.data_types as data_types
