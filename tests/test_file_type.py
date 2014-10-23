f4c47707a5e75c4ecb5928a2e720dc0827244b62
chris.barker <chris.barker@noaa.gov>
2014-10-23 16:21:45 -0700

    added identifier for nc_particles, and tests for Ugrid and nc_particles.

--- tests/test_file_type.py
+++ tests/test_file_type.py
@@ -15,26 +15,27 @@ from maproom.file_type import binary
 from peppy2.utils.file_guess import FileGuess
 
 this_dir = os.path.split(os.path.abspath(__file__))[0]
-test_data_dir = os.path.normpath(os.path.join(this_dir,"../../TestData/")) 
+test_data_dir = os.path.normpath(os.path.join(this_dir,"../TestData/")) 
 
-def test_identify_nc_particles():
-    """ make sure it works with a netcdf3 (CDF) format file """
-    recognizer = binary.NC_ParticleRecognizer()
-    guess = FileGuess( os.path.join(test_data_dir, "NC_particles", "Mobile_test.nc") )
 
-    assert recognizer.identify(guess) == binary.NC_ParticleRecognizer.id
+## fixme -- need test to make sure that only one identifier returns for each file.
+
+## Auto-generated tests from various file types, etc.
+FILES = [ ( (os.path.join(test_data_dir, "NC_particles", "Mobile_test.nc") ), binary.NC_ParticleRecognizer ),
+          ( (os.path.join(test_data_dir, "UGrid", "21_triangles.nc") ), binary.HDF5Recognizer ),
+        ]
+
+def test_identify():
+    for filename, identifier in  FILES:
+        yield check_filetype, filename, identifier
+
+def check_filetype(filename, identifier):
+    recognizer = identifier()
+    guess = FileGuess( filename)
+    assert recognizer.identify(guess) == identifier.id
 
 def test_identify_nc_particles_ugrid():
     """ make sure it rejects a ugrid netcdf file """
     recognizer = binary.NC_ParticleRecognizer()
     guess = FileGuess( os.path.join(test_data_dir, "UGrid", "21_triangles.nc") )
-
-
     assert recognizer.identify(guess) is None
-
-def test_identify_hdf():
-    """ make sure it works with a netcdf4 (HDF) ugrid file """
-    recognizer = binary.HDF5Recognizer()
-    guess = FileGuess( os.path.join(test_data_dir, "UGrid", "21_triangles.nc") )
-
-    assert recognizer.identify(guess) == binary.HDF5Recognizer.id
