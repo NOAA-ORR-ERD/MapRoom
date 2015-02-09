6b17fe1e07491bb37ead5b932fa64f3834bb10b4
Rob McMullen <rob.mcmullen@noaa.gov>
2015-02-09 13:13:09 -0800

    Fixed nc_particles unit tests

--- tests/test_nc_particles.py
+++ tests/test_nc_particles.py
@@ -8,18 +8,16 @@ from nose.tools import *
 
 import numpy as np
 
-#from mock import *
+from mock import maproom_dir
 
 from maproom.layers.loaders.nc_particles import nc_particles_file_loader
 
-this_dir = os.path.split(os.path.abspath(__file__))[0]
-test_data_dir = os.path.normpath(os.path.join(this_dir,"../TestData/")) 
-
-sample_nc_file = os.path.join(test_data_dir, "NC_particles/Mobile_test.nc")
+sample_nc_file = os.path.join(maproom_dir, "TestData/NC_particles/Mobile_test.nc")
 
 def test_iterator():
     for i, data in enumerate( nc_particles_file_loader(sample_nc_file) ):
         print "next time step:"
-        assert data['f_points'].shape == (1000, 2)
+        points, status_codes, time = data
+        assert points.shape == (1000, 2)
     assert i == 24
 
