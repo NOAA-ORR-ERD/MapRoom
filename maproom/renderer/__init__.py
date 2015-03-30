f5e4ffe0c671ffdf1cec0095ecfab7cff5a73d09
Rob McMullen <rob.mcmullen@noaa.gov>
2015-03-30 14:14:33 -0700

    cleaned up render importing * gl directory now only holds common routines * gl_old directory will be removed once convinced of correct refactoring

--- maproom/renderer/__init__.py
+++ maproom/renderer/__init__.py
@@ -1,13 +1,6 @@
 driver="gl_immediate"
 
-if driver == "gl":
-    from .gl.renderer_driver import RendererDriver
-    from .gl.layer_renderer import LayerRenderer
-    from .gl.parser import *
-    from .gl.color import color_to_int, int_to_color
-    import gl.data_types as data_types
-    from .gl.base_canvas import BaseCanvas
-elif driver == "gl_immediate":
+if driver == "gl_immediate":
     from .gl.color import color_to_int, int_to_color
     import gl.data_types as data_types
     from .gl_immediate.base_canvas import BaseCanvas
