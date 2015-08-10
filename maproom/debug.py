3fa56347889e045dbfe7d5b24a62098e923461d1
Rob McMullen <rob.mcmullen@noaa.gov>
2015-08-10 10:36:06 -0700

    Added AnnotationLayer as parent for --debug-objects * added icon to list of objects created

--- maproom/debug.py
+++ maproom/debug.py
@@ -1,14 +1,13 @@
 def debug_objects(lm):
     import layers
-#    a = layers.OverlayImageObject(manager=lm)
-#    a.set_location((-16.6637485204,-1.40163099748))
-#    a.set_style(lm.default_style)
-#    lm.insert_layer([2], a)
-#    
+    
+    a = layers.AnnotationLayer(manager=lm)
+    lm.insert_layer([2], a)
+    
     a = layers.OverlayTextObject(manager=lm)
     a.set_location((6.6637485204,5.40163099748))
     a.set_style(lm.default_style)
-    lm.insert_layer([2], a)
+    lm.insert_layer([2, 999], a)
     
     #a = layers.ScaledImageObject(manager=lm)
     b = layers.RectangleVectorObject(manager=lm)
@@ -16,14 +15,14 @@ def debug_objects(lm):
         (-16.6637485204,-1.40163099748),
         (9.65688930428,-19.545688433))
     b.set_style(lm.default_style)
-    lm.insert_layer([2], b)
+    lm.insert_layer([2, 999], b)
     
     c = layers.LineVectorObject(manager=lm)
     c.set_opposite_corners((0,0), (0,3))
     c.copy_control_point_from(0, a, 0)
     c.copy_control_point_from(1, b, 1)
     c.set_style(lm.default_style)
-    lm.insert_layer([2], c)
+    lm.insert_layer([2, 999], c)
     lm.set_control_point_link(c, 0, a, 0)
     lm.update_linked_control_points()
     lm.set_control_point_link(c, 1, b, 1)
@@ -33,7 +32,7 @@ def debug_objects(lm):
     c.set_opposite_corners((-20,10), (0,3))
     c.copy_control_point_from(1, b, 3)
     c.set_style(lm.default_style)
-    lm.insert_layer([2], c)
+    lm.insert_layer([2, 999], c)
     lm.set_control_point_link(c, 1, b, 3)
     lm.update_linked_control_points()
     
@@ -41,7 +40,7 @@ def debug_objects(lm):
     c.set_opposite_corners((-15,10), (0,3))
     c.copy_control_point_from(1, b, 3)
     c.set_style(lm.default_style)
-    lm.insert_layer([2], c)
+    lm.insert_layer([2, 999], c)
     lm.set_control_point_link(c, 1, b, 3)
     lm.update_linked_control_points()
     
@@ -49,7 +48,7 @@ def debug_objects(lm):
     c.set_opposite_corners((-10,10), (0,3))
     c.copy_control_point_from(1, b, 3)
     c.set_style(lm.default_style)
-    lm.insert_layer([2], c)
+    lm.insert_layer([2, 999], c)
     lm.set_control_point_link(c, 1, b, 3)
     lm.update_linked_control_points()
     
@@ -63,4 +62,10 @@ def debug_objects(lm):
         ])
     a.set_style(lm.default_style)
     a.style.fill_style = 0
-    lm.insert_layer([2], a)
+    lm.insert_layer([2, 999], a)
+    
+    a = layers.OverlayIconObject(manager=lm)
+    a.set_location((20, -10))
+    a.set_style(lm.default_style)
+    a.style.icon_marker = 186
+    lm.insert_layer([2, 999], a)
