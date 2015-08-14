3ae3845d667d771323badaa222b47786a236660d
Rob McMullen <rob.mcmullen@noaa.gov>
2015-08-14 15:34:55 -0700

    Added first ReportLab code to create a hello world in a pdf

--- maproom/renderer/pdf/pdf_canvas.py
+++ maproom/renderer/pdf/pdf_canvas.py
@@ -4,6 +4,11 @@ import time
 import math
 import numpy as np
 
+import wx
+
+from reportlab.pdfgen import canvas
+import reportlab.lib.pagesizes as pagesizes
+
 from renderer import ReportLabRenderer
 import maproom.library.rect as rect
 
@@ -11,6 +16,11 @@ from .. import NullPicker, BaseCanvas
 
 import logging
 log = logging.getLogger(__name__)
+
+paper_size_map = {
+    wx.PAPER_LETTER: pagesizes.letter,
+    wx.PAPER_A4: pagesizes.A4,
+    }
 
 
 class PDFCanvas(BaseCanvas):
@@ -26,8 +36,22 @@ class PDFCanvas(BaseCanvas):
     def new_renderer(self, layer):
         return ReportLabRenderer(self, layer)
     
+    def get_page_size(self):
+        print_data = self.project.task.print_data
+        id = print_data.GetPaperId()
+        size = paper_size_map[id]
+        if print_data.GetOrientation() == wx.LANDSCAPE:
+            size = (size[1], size[0])
+        return size
+    
     def prepare_screen_viewport(self):
-        pass
+        print self.screen_rect
+        pagesize = self.get_page_size()
+        print pagesize
+        c = canvas.Canvas("maproom.pdf", pagesize=pagesize)
+        c.drawString(100,100,"Hello MapRoom!")
+        c.showPage()
+        c.save()
 
     def set_screen_rendering_attributes(self):
         pass
