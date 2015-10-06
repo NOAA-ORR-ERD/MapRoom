025c52cd0096d0e373c20ca4f510c15c93b82e65
Rob McMullen <rob.mcmullen@noaa.gov>
2015-10-05 21:39:41 -0700

    Changed --debug-objects to put scale in front of tile layer * added circle object

--- maproom/debug.py
+++ maproom/debug.py
@@ -2,12 +2,12 @@ def debug_objects(lm):
     import layers
     
     a = layers.AnnotationLayer(manager=lm)
-    lm.insert_layer([2], a)
+    lm.insert_layer([3], a)
     
     a = layers.OverlayTextObject(manager=lm)
     a.set_location((6.6637485204,5.40163099748))
     a.set_style(lm.default_style)
-    lm.insert_layer([2, 999], a)
+    lm.insert_layer([3, 999], a)
     
     #a = layers.ScaledImageObject(manager=lm)
     b = layers.RectangleVectorObject(manager=lm)
@@ -15,7 +15,14 @@ def debug_objects(lm):
         (-16.6637485204,-1.40163099748),
         (9.65688930428,-19.545688433))
     b.set_style(lm.default_style)
-    lm.insert_layer([2, 999], b)
+    lm.insert_layer([3, 999], b)
+    
+    b = layers.CircleVectorObject(manager=lm)
+    b.set_opposite_corners(
+        (14.0, 4.0),
+        (18.0, 8.0))
+    b.set_style(lm.default_style)
+    lm.insert_layer([3, 999], b)
     
     c = layers.LineVectorObject(manager=lm)
     c.set_opposite_corners((0,0), (0,3))
@@ -24,7 +31,7 @@ def debug_objects(lm):
     c.set_style(lm.default_style)
     c.style.line_start_marker = 1
     c.style.line_end_marker = 2
-    lm.insert_layer([2, 999], c)
+    lm.insert_layer([3, 999], c)
     lm.set_control_point_link(c, 0, a, 0)
     lm.update_linked_control_points()
     lm.set_control_point_link(c, 1, b, 1)
@@ -36,7 +43,7 @@ def debug_objects(lm):
     c.set_style(lm.default_style)
     c.style.line_start_marker = 1
     c.style.line_end_marker = 2
-    lm.insert_layer([2, 999], c)
+    lm.insert_layer([3, 999], c)
     lm.set_control_point_link(c, 1, b, 3)
     lm.update_linked_control_points()
     
@@ -46,7 +53,7 @@ def debug_objects(lm):
     c.set_style(lm.default_style)
     c.style.line_start_marker = 1
     c.style.line_end_marker = 2
-    lm.insert_layer([2, 999], c)
+    lm.insert_layer([3, 999], c)
     lm.set_control_point_link(c, 1, b, 3)
     lm.update_linked_control_points()
     
@@ -56,7 +63,7 @@ def debug_objects(lm):
     c.set_style(lm.default_style)
     c.style.line_start_marker = 1
     c.style.line_end_marker = 2
-    lm.insert_layer([2, 999], c)
+    lm.insert_layer([3, 999], c)
     lm.set_control_point_link(c, 1, b, 3)
     lm.update_linked_control_points()
     
@@ -70,7 +77,7 @@ def debug_objects(lm):
         ])
     a.set_style(lm.default_style)
     a.style.fill_style = 0
-    lm.insert_layer([2, 999], a)
+    lm.insert_layer([3, 999], a)
     
     a = layers.PolygonObject(manager=lm)
     a.set_points([
@@ -83,13 +90,13 @@ def debug_objects(lm):
     a.set_style(lm.default_style)
     a.style.line_stipple = 0
     a.style.fill_color = a.style.default_colors[2]
-    lm.insert_layer([2, 999], a)
+    lm.insert_layer([3, 999], a)
     
     a = layers.OverlayIconObject(manager=lm)
     a.set_location((20, -10))
     a.set_style(lm.default_style)
     a.style.icon_marker = 186
-    lm.insert_layer([2, 999], a)
+    lm.insert_layer([3, 999], a)
     
     a = layers.TileLayer(manager=lm)
-    lm.insert_layer([3], a)
+    lm.insert_layer([4], a)
