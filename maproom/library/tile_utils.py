a0d6bfc832111ce43f8a5d2aa4eec0b3e373fd4a
Rob McMullen <rob.mcmullen@noaa.gov>
2016-04-06 14:31:29 -0700

    Added more ESRI tile services

--- maproom/library/tile_utils.py
+++ maproom/library/tile_utils.py
@@ -346,10 +346,18 @@ class BackgroundTileDownloader(BackgroundHttpMultiDownloader):
         if cls.cached_known_tile_server is None:
             cls.cached_known_tile_server = [
 #                LocalTileHost("Blank"),
+                
+                # ESRI services listed here: http://server.arcgisonline.com/ArcGIS/rest/services/
                 OpenTileHostYX("ESRI Topographic", ["http://server.arcgisonline.com/ArcGIS/rest/services/World_Topo_Map/MapServer/tile/"], suffix=""),
-                OpenTileHostYX("ESRI Imagery", ["http://server.arcgisonline.com/ArcGIS/rest/services/World_Imagery/MapServer/tile/"], suffix=""),
-                OpenTileHostYX("ESRI Street Map", ["http://server.arcgisonline.com/ArcGIS/rest/services/ESRI_StreetMap_World_2D/MapServer/tile/"], suffix=""),
-                OpenTileHostYX("ESRI Ocean", ["http://server.arcgisonline.com/ArcGIS/rest/services/Ocean/World_Ocean_Base/MapServer/tile/"], suffix=""),
+                OpenTileHostYX("ESRI USA Topographic", ["http://server.arcgisonline.com/ArcGIS/rest/services/USA_Topo_Maps/MapServer/tile/"], suffix=""),
+                OpenTileHostYX("ESRI Ocean Base", ["http://server.arcgisonline.com/ArcGIS/rest/services/Ocean/World_Ocean_Base/MapServer/tile/"], suffix=""),
+                OpenTileHostYX("ESRI Ocean Reference", ["http://server.arcgisonline.com/ArcGIS/rest/services/Ocean/World_Ocean_Reference/MapServer/tile/"], suffix=""),
+                OpenTileHostYX("ESRI Terrain Base", ["http://server.arcgisonline.com/ArcGIS/rest/services/World_Terrain_Base/MapServer/tile/"], suffix=""),
+                OpenTileHostYX("ESRI Satellite Imagery", ["http://server.arcgisonline.com/ArcGIS/rest/services/World_Imagery/MapServer/tile/"], suffix=""),
+                OpenTileHostYX("ESRI Street Map", ["http://server.arcgisonline.com/ArcGIS/rest/services/World_Street_Map/MapServer/tile/"], suffix=""),
+                OpenTileHostYX("ESRI NatGeo Topographic", ["http://server.arcgisonline.com/ArcGIS/rest/services/NatGeo_World_Map/MapServer/tile/"], suffix=""),
+                OpenTileHostYX("ESRI Shaded Relief", ["http://server.arcgisonline.com/ArcGIS/rest/services/World_Shaded_Relief/MapServer/tile/"], suffix=""),
+                
                 OpenTileHost("MapQuest", ["http://otile1.mqcdn.com/tiles/1.0.0/osm/", "http://otile2.mqcdn.com/tiles/1.0.0/osm/", "http://otile3.mqcdn.com/tiles/1.0.0/osm/", "http://otile4.mqcdn.com/tiles/1.0.0/osm/"]),
                 OpenTileHost("MapQuest Satellite", ["http://otile1.mqcdn.com/tiles/1.0.0/sat/", "http://otile2.mqcdn.com/tiles/1.0.0/sat/", "http://otile3.mqcdn.com/tiles/1.0.0/sat/", "http://otile4.mqcdn.com/tiles/1.0.0/sat/"]),
                 OpenTileHost("OpenStreetMap", ["http://a.tile.openstreetmap.org/", "http://b.tile.openstreetmap.org/", "http://c.tile.openstreetmap.org/"]),
