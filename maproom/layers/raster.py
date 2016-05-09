be098fc2c285b6c6a61b54be5d18007a3fb0711a
Rob McMullen <rob.mcmullen@noaa.gov>
2016-05-09 14:20:56 -0700

    Fixed #679: ignore projection changes if only vector layers present

--- maproom/layers/raster.py
+++ maproom/layers/raster.py
@@ -64,8 +64,9 @@ class RasterLayer(ProjectedLayer):
         raster_layers = self.manager.count_raster_layers()
         vector_layers = self.manager.count_vector_layers()
         
-        if raster_layers == 0 and vector_layers == 0:
+        if raster_layers == 0:
             self.manager.dispatch_event('projection_changed', self)
+            return
         currently_merc = self.manager.project.layer_canvas.projection.srs.find("+proj=merc") != -1
         currently_longlat = self.manager.project.layer_canvas.projection.srs.find("+proj=longlat") != -1
         incoming_merc = self.image_data.projection.srs.find("+proj=merc") != -1
