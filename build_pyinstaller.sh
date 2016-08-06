2dd1aa5bc7968bd70f2764031dee34c4268f688d
Rob McMullen <rob.mcmullen@noaa.gov>
2016-08-05 18:13:28 -0700

    Added more fixes and comments to pyinstaller build script

--- build_pyinstaller.sh
+++ build_pyinstaller.sh
@@ -4,15 +4,26 @@ set -v
 echo $CONDA_DEFAULT_ENV
 echo $PATH
 
-TARGET=MapRoom27
-cp maproom.py $TARGET.py
-rm -rf build/$TARGET dist/$TARGET dist/$TARGET.app
-pyinstaller -y -i maproom/icons/maproom.icns --osx-bundle-identifier gov.noaa.maproom --debug --additional-hooks-dir=pyinstaller --windowed $TARGET.py
+# can't use MapRoom because maproom is a directory name and the filesystem is
+# case-insensitive
+BUILD_TARGET=MapRoom_app
 
-# Manual fixes
-cp pyinstaller/Info.plist dist/$TARGET.app/Contents
+# Use the old py2app name in case users have symlinks or something and they are
+# updating in place
+FINAL_TARGET=Maproom
 
-cd dist/$TARGET.app/Contents/MacOS/
+cp maproom.py $BUILD_TARGET.py
+rm -rf build/$BUILD_TARGET dist/$BUILD_TARGET dist/$BUILD_TARGET.app dist/$FINAL_TARGET.app
+pyinstaller -y -i maproom/icons/maproom.icns --osx-bundle-identifier gov.noaa.maproom --debug --additional-hooks-dir=pyinstaller --windowed $BUILD_TARGET.py
+
+#### Configuration fixes
+
+# use more detailed Info.plist than the one created by pyinstaller
+cp pyinstaller/Info.plist dist/$BUILD_TARGET.app/Contents
+
+#### Library fixes
+
+cd dist/$BUILD_TARGET.app/Contents/MacOS/
 
 # fixup the duplicate wxPython libs
 rm libwx_osx_cocoau-3.0.dylib 
@@ -21,6 +32,9 @@ ln -s libwx_osx_cocoau-3.0.0.2.0.dylib libwx_osx_cocoau-3.0.dylib
 # fixup the missing geos library
 ln -s libgeos_c.1.dylib libgeos_c.dylib
 
-# CLEANUP
+#### CLEANUP
 cd ../../../..
-#rm $TARGET.py
+rm $BUILD_TARGET.py
+
+# Make it the same application name as the old py2app name
+mv dist/$BUILD_TARGET.app dist/$FINAL_TARGET.app
