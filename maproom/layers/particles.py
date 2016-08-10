Fixed particle loading off-by-one error in status codes

maproom/layers/particles.py
@@ -133,7 +133,7 @@ class ParticleLayer(PointBaseLayer):
     def set_data(self, f_points, status_codes):
         PointBaseLayer.set_data(self, f_points)
         # force status codes to fall into range of valid colors
-        status_codes = np.clip(status_codes, 0, np.alen(self.status_code_to_color))
+        status_codes = np.clip(status_codes, 0, np.alen(self.status_code_to_color) - 1)
         colors = self.status_code_to_color[status_codes]
         self.points.color = colors
         