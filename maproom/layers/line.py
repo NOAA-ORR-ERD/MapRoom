Fixed #716: added unit conversion when merging layers

maproom/layers/line.py
@@ -406,9 +406,9 @@ class LineLayer(PointLayer):
         self.line_segment_indexes = np.delete(self.line_segment_indexes, l_s_i, 0)
         return undo
 
-    def merge_from_source_layers(self, layer_a, layer_b):
+    def merge_from_source_layers(self, layer_a, layer_b, depth_unit=""):
         # Use superclass for points
-        super(LineLayer, self).merge_from_source_layers(layer_a, layer_b)
+        super(LineLayer, self).merge_from_source_layers(layer_a, layer_b, depth_unit)
 
         n = len(layer_a.line_segment_indexes) + len(layer_b.line_segment_indexes)
         self.line_segment_indexes = self.make_line_segment_indexes(n)