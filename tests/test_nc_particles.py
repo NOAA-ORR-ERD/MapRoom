7f9a6ce2b465e58ca1d3c09f8a2876c6f5c2eec7
Rob McMullen <rob.mcmullen@noaa.gov>
2017-01-11 23:28:51 -0800

    Updated test_nc_particles

--- tests/test_nc_particles.py
+++ tests/test_nc_particles.py
@@ -14,7 +14,7 @@ sample_nc_file = os.path.join(maproom_dir, "TestData/NC_particles/Mobile_test.nc
 
 def test_iterator():
     for i, data in enumerate( nc_particles_file_loader(sample_nc_file) ):
-        points, status_codes, status_code_map, time = data
+        points, status_codes, status_code_map, time, warnings = data
         print "next time step:", points.shape
         assert points.shape == (100, 2)
     assert i == 12
