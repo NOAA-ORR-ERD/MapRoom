4f4b2ae4854e3376efa50adca4e52ebda2b3e401
Rob McMullen <rob.mcmullen@noaa.gov>
2017-10-30 13:15:28 -0700

    Fixed error when saving over an existing file; was attempting to use regular file open with a URL

--- maproom/layer_manager.py
+++ maproom/layer_manager.py
@@ -1114,60 +1114,63 @@ class LayerManager(BaseDocument):
         log.debug("extra json data")
         log.debug(str(extra_json_data))
         try:
-            zf = zipfile.ZipFile(file_path, mode='w', compression=zipfile.ZIP_DEFLATED)
-            zf.writestr("extra json data", json.dumps(extra_json_data))
-            for index, layer in layer_info:
-                zip_root = "/".join([str(a) for a in index]) + "/"
-                log.debug("index=%s, layer=%s, path=%s" % (index, layer, layer.file_path))
-                data = layer.serialize_json(index)
-                if data is not None:
-                    # only store extra files for layers that aren't
-                    # encoded entirely in the JSON
-                    paths = layer.extra_files_to_serialize()
-                    if paths:
-                        # point to reparented data file
-                        basename = os.path.basename(paths[0])
-                        data['url'] = zip_root + basename
+            with fsopen(file_path, "wb") as fh:
+                zf = zipfile.ZipFile(fh, mode='w', compression=zipfile.ZIP_DEFLATED)
+                zf.writestr("extra json data", json.dumps(extra_json_data))
+                for index, layer in layer_info:
+                    zip_root = "/".join([str(a) for a in index]) + "/"
+                    log.debug("index=%s, layer=%s, path=%s" % (index, layer, layer.file_path))
+                    data = layer.serialize_json(index)
+                    if data is not None:
+                        # only store extra files for layers that aren't
+                        # encoded entirely in the JSON
+                        paths = layer.extra_files_to_serialize()
+                        if paths:
+                            # point to reparented data file
+                            basename = os.path.basename(paths[0])
+                            data['url'] = zip_root + basename
 
-                        # save all files into zip file
-                        for p in paths:
-                            basename = os.path.basename(p)
-                            if "://" in p:
-                                # handle URI format
-                                fs, relpath = opener.parse(p)
-                                if fs.hassyspath(relpath):
-                                    p = fs.getsyspath(relpath)
-                                else:
-                                    raise RuntimeError("Can't yet handle URIs not on local filesystem")
-                            archive_name = zip_root + basename
-                            zf.write(p, archive_name, zipfile.ZIP_STORED)
+                            # save all files into zip file
+                            for p in paths:
+                                basename = os.path.basename(p)
+                                if "://" in p:
+                                    # handle URI format
+                                    fs, relpath = opener.parse(p)
+                                    if fs.hassyspath(relpath):
+                                        p = fs.getsyspath(relpath)
+                                    else:
+                                        raise RuntimeError("Can't yet handle URIs not on local filesystem")
+                                archive_name = zip_root + basename
+                                zf.write(p, archive_name, zipfile.ZIP_STORED)
 
-                    try:
-                        text = json.dumps(data, indent=4)
-                    except Exception, e:
-                        log.error("JSON failure, layer %s: data=%s" % (layer.name, repr(data)))
-                        errors = []
-                        for k, v in data.iteritems():
-                            small = {k: v}
-                            try:
-                                _ = json.dumps(small)
-                            except Exception:
-                                errors.append((k, v))
-                        log.error("JSON failures at: %s" % ", ".join(["%s: %s" % (k, v) for k, v in errors]))
-                        return "Failed saving data in layer %s.\n\n%s" % (layer.name, e)
+                        try:
+                            text = json.dumps(data, indent=4)
+                        except Exception, e:
+                            log.error("JSON failure, layer %s: data=%s" % (layer.name, repr(data)))
+                            errors = []
+                            for k, v in data.iteritems():
+                                small = {k: v}
+                                try:
+                                    _ = json.dumps(small)
+                                except Exception:
+                                    errors.append((k, v))
+                            log.error("JSON failures at: %s" % ", ".join(["%s: %s" % (k, v) for k, v in errors]))
+                            return "Failed saving data in layer %s.\n\n%s" % (layer.name, e)
 
-                    zip_path = zip_root + "json layer description"
-                    processed = collapse_json(text, 12)
-                    zf.writestr(zip_path, processed)
+                        zip_path = zip_root + "json layer description"
+                        processed = collapse_json(text, 12)
+                        zf.writestr(zip_path, processed)
+                zf.close()
 
         except RuntimeError, e:
+            log.error("file save error: %s" % str(e))
             return "Failed saving %s: %s" % (file_path, e)
         except Exception, e:
+            import traceback
+            log.error("file save error: %s\n%s" % (str(e), traceback.format_exc()))
             return "Failed saving %s: %s" % (file_path, e)
-        finally:
-            zf.close()
-        zf = zipfile.ZipFile(file_path)
-        print("\n".join(zf.namelist()))
+        # zf = zipfile.ZipFile(file_path)
+        # print("\n".join(zf.namelist()))
         return ""
 
     def save_all_text(self, file_path, extra_json_data=None):
