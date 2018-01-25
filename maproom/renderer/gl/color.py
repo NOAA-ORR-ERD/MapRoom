Fixed #862, #863: fixed incorrect scaling of int color values that fixes both issues

maproom/renderer/gl/color.py
@@ -80,7 +80,7 @@ def int_to_color_uint8(color):
 def int_to_color_ints(color):
     c = np.uint32(color)  # handle plain python integer being passed in
     ints = np.frombuffer(c.tostring(), dtype=np.uint8)
-    return tuple(int(255 * c) for c in ints)
+    return tuple(int(c) for c in ints)
 
 
 def int_to_wx_colour(color):