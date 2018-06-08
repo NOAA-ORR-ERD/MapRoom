407a77a91b1d6585d95a2175088a4c993cbdce77
Rob McMullen <rob.mcmullen@noaa.gov>
2018-06-07 22:21:49 -0700

    Initial rendering of polygons by aggregating children of folder layer

--- maproom/layers/polygon2.py
+++ maproom/layers/polygon2.py
@@ -9,7 +9,7 @@ from traits.api import Unicode
 from ..library.scipy_ckdtree import cKDTree
 from ..library.Boundary import Boundaries
 from ..library.shapely_utils import shapely_to_polygon
-from ..renderer import data_types
+from ..renderer import color_floats_to_int, data_types
 from ..command import UndoInfo
 from ..mouse_commands import DeleteLinesCommand, MergePointsCommand
 
@@ -62,6 +62,10 @@ class PolygonParentLayer(Folder, LineLayer):
 
     selection_info_panel = ["Selected points", "Point index", "Point latitude", "Point longitude"]
 
+    @property
+    def is_renderable(self):
+        return True
+
     def has_groupable_objects(self):
         return True
 
@@ -82,4 +86,42 @@ class PolygonParentLayer(Folder, LineLayer):
                 except TypeError:
                     pass
             return str(total)
-        return None
+        return str(None)
+
+    def rebuild_renderer(self, renderer, in_place=False):
+        points = data_types.make_points(0)
+        n_rings = 0
+        ring_starts = []
+        ring_counts = []
+        point_start_index = 0
+        for child in self.get_child_layers():
+            if len(points) > 0:
+                points = np.append(points, child.points).view(np.recarray)
+            else:
+                points = child.points.copy()
+            n_rings += 1
+            ring_starts.append(point_start_index)
+            ring_counts.append(len(child.points))
+            point_start_index += len(child.points)
+        projection = self.manager.project.layer_canvas.projection
+        projected_point_data = data_types.compute_projected_point_data(points, projection)
+        renderer.set_points(projected_point_data, points.z, points.color.copy().view(dtype=np.uint8))
+        self.rings, self.point_adjacency_array = data_types.compute_rings(ring_starts, ring_counts)
+        print(f"ring list: {self.rings}")
+        print(f"points: {point_start_index}, from rings: {self.rings[-1][0] + self.rings[-1][1]}")
+
+        renderer.set_polygons(self.rings, self.point_adjacency_array)
+
+    def can_render_for_picker(self, renderer):
+        return renderer.canvas.project.layer_tree_control.is_edit_layer(self)
+
+    def render_projected(self, renderer, w_r, p_r, s_r, layer_visibility, picker):
+        log.log(5, "Rendering polygon folder layer!!! pick=%s" % (picker))
+        if picker.is_active and not self.can_render_for_picker(renderer):
+            return
+        # the rings
+        if layer_visibility["polygons"]:
+            renderer.draw_polygons(self, picker,
+                                   self.rings.color,
+                                   color_floats_to_int(0, 0, 0, 1.0),
+                                   1)
