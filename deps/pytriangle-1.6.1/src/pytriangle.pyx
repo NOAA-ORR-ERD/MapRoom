52056000b72eeef0f7372e183126095ec0c4f785
Rob McMullen <rob.mcmullen@noaa.gov>
2018-06-25 14:39:56 -0700

    Vast simplification of pytriangle.pyx: pass return data through a Queue rather than pipe serialization/deserialization

--- deps/pytriangle-1.6.1/src/pytriangle.pyx
+++ deps/pytriangle-1.6.1/src/pytriangle.pyx
@@ -2,25 +2,8 @@ import multiprocessing
 import cython
 import numpy as np
 cimport numpy as np
-cimport libc.stdlib
 
 np.import_array()
-
-cdef extern from "numpy/arrayobject.h":
-    # We only care about the flags member.
-    ctypedef struct PyArrayObject:
-        void* _ob_next
-        void* _ob_prev
-        Py_ssize_t ob_refcnt
-        void* ob_type
-        char* data
-        int nd
-        np.npy_intp* dimensions
-        np.npy_intp* strides
-        void* base
-        void* descr
-        int flags
-        void* weakreflist
 
 
 cdef extern from "defines.h":
@@ -66,7 +49,6 @@ cdef extern from "triangle.h":
     )
 
 
-cdef unsigned int TIMEOUT = 1
 cdef unsigned int PROCESSING_TIMEOUT = 10
 
 
@@ -122,207 +104,53 @@ def triangulate_simple(
     
     # if not isinstance( param_text, unicode ):
     #     raise ValueError( "requires text input, got %s" % type( param_text ) )
-    utf8_param_text = param_text.encode( 'UTF-8' )
-    
-    ( connection, child_connection ) = \
-        multiprocessing.Pipe( duplex = True )
-
+    q = multiprocessing.Queue()
     child = multiprocessing.Process(
         target = triangulate_simple_child,
-        args = ( child_connection, utf8_param_text ),
+        args = (q, param_text, points_xy, points_z, lines, hole_points_xy),
     )
     child.daemon = True
     child.start()
-
-    # Send data to be triangulated to child process.
-    connection.send( points_xy.shape[ 0 ] )
-    connection.send_bytes( points_xy )
-    connection.send_bytes( points_z )
-
-    connection.send( lines.shape[ 0 ] )
-    connection.send_bytes( lines )
-
-    connection.send( hole_points_xy.shape[ 0 ] )
-    connection.send_bytes( hole_points_xy )
-
-    # Receive data resulting from triangulation from child process. Allow
-    # longer for the initial timeout since it includes triangulation time.
-    if not connection.poll(PROCESSING_TIMEOUT):
-        cleanup_child( connection, child )
-        raise RuntimeError( "Triangulation timeout." )
-    cdef unsigned int out_point_count = <unsigned int>connection.recv()
-    # print("received %d points" % out_point_count)
-
-    cdef np.ndarray[ np.float64_t ] out_points_xy = \
-       np.ndarray( ( out_point_count * 2 ), np.float64 )
-
-    poll_with_timeout( connection, child )
-    connection.recv_bytes_into( out_points_xy )
-
-    cdef np.ndarray[ np.float32_t ] out_points_z = \
-       np.ndarray( ( out_point_count, ), np.float32 )
-
-    poll_with_timeout( connection, child )
-    connection.recv_bytes_into( out_points_z )
-
-    poll_with_timeout( connection, child )
-    cdef unsigned int out_line_count = <unsigned int>connection.recv()
-
-    cdef np.ndarray[ np.uint32_t ] out_lines = \
-       np.ndarray( ( out_line_count * 2 ), np.uint32 )
-
-    poll_with_timeout( connection, child )
-    connection.recv_bytes_into( out_lines )
-
-    poll_with_timeout( connection, child )
-    cdef unsigned int out_triangle_count = <unsigned int>connection.recv()
-
-    cdef np.ndarray[ np.uint32_t ] out_triangles = \
-       np.ndarray( ( out_triangle_count * 3 ), np.uint32 )
-
-    poll_with_timeout( connection, child )
-    connection.recv_bytes_into( out_triangles )
-
-    cleanup_child( connection, child )
-
-    return ( out_points_xy.reshape((out_point_count, 2)), out_points_z, out_lines.reshape((out_line_count, 2)), out_triangles.reshape((out_triangle_count, 3)) )
-
-
-cdef poll_with_timeout( connection, child ):
-    if not connection.poll( TIMEOUT ):
-        cleanup_child( connection, child )
-        raise RuntimeError( "Triangulation timeout." )
-
-
-cdef cleanup_child( connection, child ):
-    connection.close()
-    child.join( TIMEOUT )
-
+    child.join(PROCESSING_TIMEOUT)
     if child.is_alive():
+        # timeout reached! Kill the process
         child.terminate()
+        raise RuntimeError("Triangulation timeout.")
+    output = q.get_nowait()
+    try:
+        out_points_xy, out_points_z, out_lines, out_triangles = output
+    except ValueError:
+        raise RuntimeError("Triangulation error: no output")
+
+    return out_points_xy, out_points_z, out_lines, out_triangles
 
 
 @cython.boundscheck( False )
-def triangulate_simple_child( connection, utf8_param_text ):
+def triangulate_simple_child(
+    output_queue,
+    param_text,
+    np.ndarray[ double, ndim = 2 ] points_xy not None,
+    np.ndarray[ float ] points_z not None,
+    np.ndarray[ unsigned int, ndim = 2 ] lines not None,
+    np.ndarray[ double, ndim = 2 ] hole_points_xy not None,
+):
     """
-    Triangulate the area bounded by the lines and points received from the
-    given pipe connections. This is accomplished with quality mesh generation
-    by Delaunay refinement.
+    Call Shewchuk's Triangle library to generate the Delauney triangulation.
 
-    The point depth values are interpolated to populate the depth values of
-    the resulting output points.
-
-    This function is intended to be invoked as a separate child process.
-
-    :param connection: duplex pipe connection used to exchange data with
-                       parent process
-    :type connection: multiprocessing.Connection
-
-    The following buffers are read in order from the connection:
-
-    :param point_count: number of points to triangulate
-    :type point_count: int
-    :param points_xy: geographic points to triangulate
-    :type points_xy: buffer of type float, where each pair of elements
-                     represent the 2D coordinate of a point
-    :param points_z: depth values of geographic points
-    :type points_z: buffer of type float
-    :param line_count: number of line segments to triangulate
-    :type line_count: int
-    :param lines: line segments describing outer boundary and holes of area to
-                  triangulate
-    :type lines: buffer of type int, where each pair of elements represent
-                 segment endpoint indices in the points_xy array
-    :param hole_point_count: number of hole points to exclude from
-                             triangulation
-    :type hole_point_count: int
-    :param hole_points_xy: geographic points describing hole regions to
-                           exclude from triangulation
-    :type hole_points_xy: buffer of type float, where each pair of elements
-                          represent the 2D coordinate of any point within a
-                          hole (one point per hole region)
-
-    If the triangulation is successful, the following data and buffers are
-    written in order to the connection:
-
-    :param out_point_count: number of points resulting from triangulation
-    :type out_point_count: int
-    :param out_points_xy: geographic points resulting from triangulation
-    :type out_points_xy: buffer of type float, where each pair of elements
-                         represent the 2D coordinate of a point
-    :param out_points_z: depth values of geographic points resulting from
-                         triangulation
-    :type out_points_z: buffer of type float
-    :param out_line_count: number of line segments resulting from triangulation
-    :type out_line_count: int
-    :param out_lines: line segments describing outer boundary and holes
-                      resulting from triangulation
-    :type out_lines: buffer of type int, where each pair of elements represent
-                     segment endpoint indices in the out_points_xy array
-    :param out_triangle_count: number of triangles resulting from
-                               triangulation
-    :type out_triangle_count: int
-    :param out_triangles: triangles resulting from triangulation
-    :type out_triangles: buffer of type int, where each triple of elements
-                         represent triangle corner indices in the
-                         out_points_xy array
-
-    If the triangulation is not successful, then the child process may simply
-    terminate or even hang.
+    Must be called in its own subprocess because the library may terminate or
+    even hang if the triangulation is not successful.
     """
-    switchesP = b"pzB" + utf8_param_text # TODO: Add -a# -q# -Q
+    switchesP = b"pzB" + param_text.encode('UTF-8') # TODO: Add -a# -q# -Q
     cdef char* switches = switchesP
     cdef triangulateio in_data, out_data
 
-    # Receive incoming data from the parent process.
-    if not connection.poll( TIMEOUT ): return
-    cdef unsigned int point_count = <unsigned int>connection.recv()
-    # print("point count: ", point_count)
-
-    # Receive as 32-bit floats, then convert to 64-bit floats below, so the
-    # Triangle library has more precision to work with.
-    cdef np.ndarray[ np.float64_t ] points_xy = \
-       np.ndarray( ( point_count * 2 ), np.float64 )
-
-    if not connection.poll( TIMEOUT ): return
-    # print("storage: ", len(points_xy))
-    connection.recv_bytes_into( points_xy )
-
-    cdef np.ndarray[ np.float32_t ] points_z = \
-       np.ndarray( ( point_count ), np.float32 )
-
-    if not connection.poll( TIMEOUT ): return
-    connection.recv_bytes_into( points_z )
-
-    cdef np.ndarray[ np.float64_t ] points_z_64 = \
-       points_z.astype( np.float64 )
-
-    if not connection.poll( TIMEOUT ): return
-    c = connection.recv()
-    # print("line count: ", c)
-    cdef unsigned int line_count = <unsigned int>c
-
-    cdef np.ndarray[np.uint32_t] lines = \
-       np.ndarray( ( line_count * 2 ), np.uint32 )
-
-    # print("lines: ", len(lines), line_count)
-    if not connection.poll( TIMEOUT ): return
-    connection.recv_bytes_into( lines )
-
-    if not connection.poll( TIMEOUT ): return
-    cdef unsigned int hole_point_count = <unsigned int>connection.recv()
-
-    cdef np.ndarray[ np.float64_t ] hole_points_xy = \
-       np.ndarray( ( hole_point_count * 2 ), np.float64 )
-
-    if not connection.poll( TIMEOUT ): return
-    connection.recv_bytes_into( hole_points_xy )
+    cdef np.ndarray[np.float64_t] points_z_64 = points_z.astype(np.float64)
 
     # Pass the data to Triangle and triangulate with it.
     in_data.pointlist = <double*>points_xy.data
     in_data.pointattributelist = <double*>points_z_64.data
     in_data.pointmarkerlist = NULL
-    in_data.numberofpoints = point_count
+    in_data.numberofpoints = <int>points_xy.shape[0]
     in_data.numberofpointattributes = 1
     in_data.trianglelist = NULL
     in_data.triangleattributelist = NULL
@@ -332,9 +160,9 @@ def triangulate_simple_child( connection, utf8_param_text ):
     in_data.numberoftriangleattributes = 0
     in_data.segmentlist = <int*>lines.data # Note the conversion to signed!
     in_data.segmentmarkerlist = NULL
-    in_data.numberofsegments = lines.shape[ 0 ]
+    in_data.numberofsegments = <int>lines.shape[0]
     in_data.holelist = <double*>hole_points_xy.data
-    in_data.numberofholes = hole_points_xy.shape[ 0 ]
+    in_data.numberofholes = <int>hole_points_xy.shape[0]
     in_data.regionlist = NULL
     in_data.numberofregions = 0
 
@@ -345,51 +173,12 @@ def triangulate_simple_child( connection, utf8_param_text ):
 
     triangulate( switches, &in_data, &out_data, NULL )
 
-    # Create NumPy arrays from the output data, giving NumPy ownership of (and
-    # responsibility for ultimately deallocating) that memory. Convert the
-    # 64-bit floats to 32-bit floats before sending out.
-    ## fixme: why run triangle in 64 bit mode?
-    cdef np.npy_intp* out_points_xy_dims = [ out_data.numberofpoints, 2 ]
-    cdef np.ndarray[ double, ndim = 2 ] out_points_xy = \
-        np.PyArray_SimpleNewFromData(
-            2, out_points_xy_dims, np.NPY_DOUBLE, out_data.pointlist,
-        ).astype( np.float64 )
-    ( <PyArrayObject*>out_points_xy )[ 0 ].flags |= np.NPY_OWNDATA
+    out_points_xy = np.PyArray_SimpleNewFromData(2, [out_data.numberofpoints, 2], np.NPY_DOUBLE, out_data.pointlist)
 
-    cdef np.npy_intp* out_points_z_dims = [ out_data.numberofpoints ]
-    cdef np.ndarray[ float ] out_points_z = \
-        np.PyArray_SimpleNewFromData(
-            1, out_points_z_dims, np.NPY_DOUBLE, out_data.pointattributelist,
-        ).astype( np.float32 )
-    ( <PyArrayObject*>out_points_z )[ 0 ].flags |= np.NPY_OWNDATA
+    out_points_z = np.PyArray_SimpleNewFromData(1, [out_data.numberofpoints], np.NPY_DOUBLE, out_data.pointattributelist).astype(np.float32)
 
-    cdef np.npy_intp* out_lines_dims = [
-        out_data.numberofsegments, 2,
-    ]
-    cdef np.ndarray[ int, ndim = 2 ] out_lines = \
-        np.PyArray_SimpleNewFromData(
-            2, out_lines_dims, np.NPY_INT, out_data.segmentlist,
-        )
-    ( <PyArrayObject*>out_lines )[ 0 ].flags |= np.NPY_OWNDATA
+    out_lines = np.PyArray_SimpleNewFromData(2, [out_data.numberofsegments, 2], np.NPY_INT, out_data.segmentlist)
 
-    cdef np.npy_intp* out_triangles_dims = [
-        out_data.numberoftriangles, out_data.numberofcorners
-    ]
-    cdef np.ndarray[ int, ndim = 2 ] out_triangles = \
-        np.PyArray_SimpleNewFromData(
-            2, out_triangles_dims, np.NPY_INT, out_data.trianglelist,
-        )
-    ( <PyArrayObject*>out_triangles )[ 0 ].flags |= np.NPY_OWNDATA
+    out_triangles = np.PyArray_SimpleNewFromData(2, [out_data.numberoftriangles, out_data.numberofcorners], np.NPY_INT, out_data.trianglelist)
 
-    # Send outgoing data to the parent process.
-    connection.send( out_points_xy.shape[ 0 ] )
-    connection.send_bytes( out_points_xy )
-    connection.send_bytes( out_points_z )
-
-    connection.send( out_lines.shape[ 0 ] )
-    connection.send_bytes( out_lines )
-
-    connection.send( out_triangles.shape[ 0 ] )
-    connection.send_bytes( out_triangles )
-
-    connection.close()
+    output_queue.put((out_points_xy, out_points_z, out_lines, out_triangles))
