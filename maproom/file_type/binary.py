a22130ecfc8944cede05e7ff5338eaae7e8d10ad
Rob McMullen <rob.mcmullen@noaa.gov>
2018-10-23 16:09:12 -0700

    Prevent NC particle files from being recognized a generic netcdf files

--- maproom/file_type/binary.py
+++ maproom/file_type/binary.py
@@ -72,7 +72,8 @@ class UGRID_Recognizer(RecognizerBase):
         byte_stream = guess.get_bytes()
         # check if it is either HDF or CDF
         if byte_stream[:3] == b"CDF" or byte_stream[0:8] == b"\211HDF\r\n\032\n":
-            if (b'cf_role' in byte_stream) and (b'mesh_topology' in byte_stream):
+            if not ((b'feature_type' in byte_stream) and (b'particle_trajector' in byte_stream)):
+                # only recognize here if wouldn't be recognized as a particle file
                 return self.id
 
 
