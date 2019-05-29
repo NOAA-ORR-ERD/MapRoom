f8a126f773068c4f6e829e0b2182b3c336892230
Rob McMullen <rob.mcmullen@noaa.gov>
2019-05-29 07:21:08 -0700

    Added show_focus debug menu item

--- maproom/editor.py
+++ maproom/editor.py
@@ -200,6 +200,7 @@ class ProjectEditor(SawxEditor):
                 "garbage_objects",
                 "raise_exception",
                 "test_progress",
+                "show_focus",
             ],
         ],
     ]
